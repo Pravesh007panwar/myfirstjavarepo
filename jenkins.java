18588f957230e0156f55ea8cb9cb1e4fce2a575d
unknown <pravesh256@gmail.com>
2021-06-12 13:19:25 +0530

    today I have completed the Deployment of jenkins

---
+++ jenkins.java
@@ -0,0 +1,14 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 8EED-D56A
+
+ Directory of C:\selenium\github
+
+12-06-2021  13:16    <DIR>          .
+12-06-2021  13:16    <DIR>          ..
+11-06-2021  13:39               118 hello.java
+12-06-2021  13:16                 0 jenkins.java
+12-06-2021  01:37               479 read.html
+12-06-2021  01:36               432 read.text
+11-06-2021  17:17               415 Simple.class
+               5 File(s)          1,444 bytes
+               2 Dir(s)  409,183,641,600 bytes free
